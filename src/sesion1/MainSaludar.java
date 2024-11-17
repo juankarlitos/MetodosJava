62d1d54c46325a9c25049fb316a7c1781d7257ae
juankarlitos <juankarlito113@gmail.com>
2024-11-17 19:01:55 -0400

    agergacion de codigo al metodo

---
+++ src/sesion1/MainSaludar.java
@@ -0,0 +1,15 @@
+package sesion1;
+
+public class MainSaludar {
+
+    public static void main(String[] args) {
+        Saludar saludar = new Saludar();
+
+        saludar.saludar();
+        saludar.saludar();
+        saludar.saludar();
+        saludar.saludar();
+
+
+    }
+}
--- src/sesion1/Saludar.java
+++ src/sesion1/Saludar.java
@@ -5,10 +5,9 @@ import java.net.StandardSocketOptions;
 public class Saludar {
 
     public void  saludar(){
-        System.out.println("hola mi nombre es ");
-
-
-
+        System.out.println("hola a todos desde Java");
     }
+
+
 
 }
