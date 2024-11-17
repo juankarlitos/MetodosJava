7ccc476766bd48cf0035faeb89c0acb3a462c6a7
juankarlitos <juankarlito113@gmail.com>
2024-11-17 18:08:01 -0400

    Creacion nueva rama de metodos java

---
+++ src/sesion1/Saludar.java
@@ -0,0 +1,14 @@
+package sesion1;
+
+import java.net.StandardSocketOptions;
+
+public class Saludar {
+
+    public void  saludar(){
+        System.out.println("hola mi nombre es ");
+
+
+
+    }
+
+}
